b643e4fb86e2ab8a3cbc03774d1bff3b757f7409
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-07 19:00:05 +0300

    projec magic ball

---
+++ magic-ball.php
@@ -0,0 +1,68 @@
+<?php
+function magic8Ball() {
+  echo "Enter your question to our awesome Magic Ball!\n";
+  $question = readLine(">> ");
+  echo "Your question is: ${question}\n";
+
+  $choice = rand(0, 19);
+  echo "\n";
+  switch ($choice) {
+    case 0:
+      echo "It is certain.";
+      break;
+    case 1:
+      echo "It is decidedly so.";
+      break;
+    case 2:
+      echo "Without a doubt.";
+      break;
+    case 3:
+      echo "Yes - definitely.";
+      break;
+    case 4:
+      echo "You may rely on it.";
+      break;
+    case 5:
+      echo "As I see it, yes.";
+      break;
+    case 6:
+      echo "Most likely.";
+      break;
+    case 7:
+      echo "Outlook good.";
+      break;
+    case 8:
+      echo "Yes";
+      break;
+    case 9:
+      echo "Signs point to yes.";
+      break;
+    case 10:
+      echo "Reply hazy, try again.";
+      break;
+    case 11:
+      echo "Ask again later.";
+      break;
+    case 12:
+      echo "Better not tell you now.";
+      break;
+    case 13:
+      echo "Cannot predict now.";
+      break;
+    case 14:
+      echo "Concentrate and ask again.";
+      break;
+    case 15:
+      echo "Don't count on it.";
+      break;
+    case 16:
+      echo "My reply is no.";
+      break;
+    default:
+      echo "My sources say no.";
+      break;
+  }
+  echo "\n";
+}
+
+magic8Ball();
